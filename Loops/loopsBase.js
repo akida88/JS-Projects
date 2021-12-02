e58925c14d0a10f82715c1225b2fad6f87d1d6ed
Akida <tinakrysteva@gmail.com>
2021-12-02 16:18:49 +0200

    loops

---
+++ Loops/loopsBase.js
@@ -0,0 +1,12 @@
+// write for loops
+// write while loops
+// avoid infinite loops
+// iterate over arrays and objects
+
+// Loops: doing things repeatedly
+// Loops allow us to repeat code
+// = Print 'hello' 10 times
+// = sum all numbers in an array
+
+// there are multiple types: for loop, while loop, for ...of loop, for ...in loop
+
