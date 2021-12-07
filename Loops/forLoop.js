7f3143d3969d2754b921296b5710dc5f31d89d96
Akida <tinakrysteva@gmail.com>
2021-12-07 17:27:25 +0200

    for Loops

--- Loops/forLoop.js
+++ Loops/forLoop.js
@@ -5,6 +5,48 @@ for (
 )
 
 // similar in the conditional, but not how it's working
-if(){
+if(){ // the code inside the {} will only run if the condition in the () is true
 
 }
+
+
+
+// we define a variable with the for loop
+// initial value
+// when to run the loop
+// how to change value each time
+
+
+  //variable; condition; operation that is performed on the i
+  //start at 1; stop at 10; add 1 each time
+for(let i = 1; i <= 10; i++){
+   console.log("Hello:", i);
+}
+
+for(let i = 1; i <= 10; i+=3){ // 1 > 4 > 7 > 10 > 13
+   console.log("Hello:", i);
+}
+
+
+
+// 3 pieces to a for loop:
+// 1 - Variable declaration
+// 2 - The condition
+// 3 - Update the loop variable
+
+// STEP 1: 'let i = 1' - start i at 1
+// STEP 2: 'i <= 10' - run the loop as long as i <= 10
+// STEP 3: 'i++' - add 1 to i each time through
+
+
+
+// Printing first 20 perfect squares:
+for (let num = 1; num <= 20; num++) {
+	console.log(`${num}x${num} = ${num * num}`);
+}
+
+// Counting DOWN from 200 by intervals of 25:
+for (let i = 200; i >= 0; i -= 25) {
+	console.log(i);
+}
+console.log('AFTER THE LOOP!');
---
+++ Loops/infiniteLoop.js
@@ -0,0 +1,14 @@
+// DO NOT run this code!
+for (let i = 20; i >= 0; i++) {
+   console.log(i);
+}
+
+// a loop that the ending condition is never met (the second part)
+
+
+for(let i = 1; i!==20; i+=2){ // it returns 1,3,5,7, etc and never meets 20 and it continue
+   console.log(i);
+} // better to use i<=20
+
+// using > and = is better than using equality with numbers
+
